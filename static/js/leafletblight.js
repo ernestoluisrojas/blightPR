a2b04042c479013faf6b4f4e680687284308e474
ernestoluisrojas <ernestoluisrojas@hotmail.com>
2015-06-30 20:10:45 -0400

    changed map tiles source

--- static/js/leafletblight.js
+++ static/js/leafletblight.js
@@ -9,11 +9,22 @@
     }).addTo(map);
     */
 
-    //OpenStreetMap.BlackAndWhite
+    /*OpenStreetMap.BlackAndWhite
     L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
     maxZoom: 18,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+    */
+
+    //Stamen_TonerLite
+    L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: 'abcd',
+    minZoom: 0,
+    maxZoom: 20,
+    ext: 'png'
+}).addTo(map);
+    
 
     var shpfile = new L.Shapefile("/static/media/Desuso.zip",{
       onEachFeature:function(feature, layer) { 
