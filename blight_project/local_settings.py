08b8ec1da7f0d8381ae75ceb1e26f6aa3b81f117
ernestoluisrojas <ernestoluisrojas@hotmail.com>
2015-07-07 20:23:03 -0400

    Update local_settings.py

--- blight_project/local_settings.py
+++ blight_project/local_settings.py
@@ -1,13 +1,2 @@
 
-#DISQUS_API_KEY = 'LeLlakcK4BPFjp8JLW5iKqCY701jUxlYzSXTlqDsrVbTfu8QZbpKv4BwVqisFLMl'
-DISQUS_API_KEY = '3lGqLZu5H2JWoj1JHGuyj3soLwlM3cZay5DJsLR1nGDwj4YpUBCT14oQiG7CNhK3'
-
-'''
-Email:
-- blightpr@code4puertorico.org
-- j5T8NZW3o-4YbrH
-
-Disqus:
-- blightpr
-- bp7miVgdG0OfXzp
-'''
+DISQUS_API_KEY = '<YOUR DISQUS API KEY>'
